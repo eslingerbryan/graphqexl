45063d4701c1789e2f3efaa2702c03cf53d4d83c
Bryan Eslinger <bryan.eslinger@liveramp.com>
2020-01-20 16:54:31 -0800

    Add mix dev task

---
+++ lib/mix/tasks/dev.ex
@@ -0,0 +1,9 @@
+defmodule Mix.Tasks.Dev do
+  @shortdoc """
+  [STUB] Run a development server with the specified schema file
+  """
+
+  def run([_schema_file]) do
+    IO.puts "[TODO] Implement a dev server environment to run here..."
+  end
+end
