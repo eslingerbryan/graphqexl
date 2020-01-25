bc7d462ce1237a9e86dcfcdec64bfbb265edb403
Bryan Eslinger <bryan.eslinger@liveramp.com>
2020-01-25 11:15:08 -0800

    Parse variables

--- lib/graphqexl/query.ex
+++ lib/graphqexl/query.ex
@@ -31,7 +31,9 @@ defmodule Graphqexl.Query do
   @opening_brace "{"
 
   @identifier "[_a-z][_a-zA-Z0-9]+"
-  @operation_pattern ~r/(?<type>#{@identifier})?\s?(?<name>#{@identifier})\(?(?<fields>.*)\)?\s\{/
+  @type_pattern "?<type>#{@identifier}"
+  @name_pattern "?<name>#{@identifier}"
+  @operation_pattern ~r/(#{@type_pattern})?\s?(#{@name_pattern})(?<arguments>\(?\(\$.*\))?\s\{/
 
   @doc """
   Execute the given `t:Graphqexl.Query.t/0`
@@ -56,26 +58,6 @@ defmodule Graphqexl.Query do
   @doc since: "0.1.0"
   @spec parse(gql) :: Query.t
   def parse(gql) when is_binary(gql) do
-    gql
-    |> String.split("\n")
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&(String.replace(&1, "\n{", "{")))
-    |> Enum.reduce(%{stack: [], current: nil, fields: [], operations: []}, &tokenize/2)
-  end
-
-  @doc """
-  Parse the given json map into a `t:Graphqexl.Query.t/0`
-
-  Returns: `t:Graphqexl.Query.t/0`
-  """
-  @doc since: "0.1.0"
-  @spec parse(json) :: Query.t
-  def parse(_json) do
-    # convert bare map to %Query{}
-  end
-
-  @doc false
-  def tokenize(line, %{stack: stack, current: current, fields: fields, operations: operations}) do
     """
     getPost(id: "foo") {
       author {
@@ -109,6 +91,26 @@ defmodule Graphqexl.Query do
       text: %{}
     }
     """
+    gql
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&(String.replace(&1, "\n{", "{")))
+    |> Enum.reduce(%{stack: [], current: nil, fields: [], operations: []}, &tokenize/2)
+  end
+
+  @doc """
+  Parse the given json map into a `t:Graphqexl.Query.t/0`
+
+  Returns: `t:Graphqexl.Query.t/0`
+  """
+  @doc since: "0.1.0"
+  @spec parse(json) :: Query.t
+  def parse(_json) do
+    # convert bare map to %Query{}
+  end
+
+  @doc false
+  def tokenize(line, %{stack: stack, current: current, fields: fields, operations: operations}) do
     case line |> String.at(-1) do
       @opening_brace ->
         if is_nil(current) do
@@ -158,20 +160,27 @@ defmodule Graphqexl.Query do
     %{type: type, name: name, arguments: arguments} =
       @operation_pattern |> Regex.named_captures(line)
 
+    parsed = arguments |> tokenize_arguments
+
+    var? = String.contains?(line, "$")
+
     %Operation{
       type: type |> String.to_atom,
       name: name,
-      arguments: arguments |> tokenize_arguments,
-      fields: %{}
+      arguments: if var? do %{} else parsed end,
+      fields: %{},
+      variables: if var? do parsed else %{} end
     }
   end
 
   @doc false
-  defp tokenize_arguments(arguments) do
-    arguments
+  defp tokenize_arguments(variables) do
+    variables
     |> String.replace(@close_argument, "")
     |> String.replace(@open_argument, "")
-    |> String.split("\n")
+    |> String.replace(",", "")
+    |> String.replace("$", "")
+    |> String.split(" ")
     |> Enum.map(&(
       %{
         name: String.split(&1, ":") |> elem(0),
