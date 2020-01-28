300878280d240a5cc3110f179c82976708adfc23
Bryan Eslinger <bryan.eslinger@liveramp.com>
2020-01-28 15:56:09 -0800

    Refactor for legibility

--- lib/graphqexl/server/plug.ex
+++ lib/graphqexl/server/plug.ex
@@ -8,16 +8,28 @@ defmodule Graphqexl.Server.Plug do
 
   def call(conn, _opts) do
     # content negotiation: application/json and application/graphql
-    {:ok, result} =
-      conn.body_params
-      |> Map.get("q")
-      |> Graphqexl.Query.parse
-      |> Graphqexl.Query.execute(conn.assigns[:schema])
-      |> (&(Jason.encode(%{data: &1.data, errors: &1.errors}))).()
-
     conn
     |> put_resp_content_type("application/json")
     # determine status
-    |> send_resp(200, result)
+    |> send_result
+  end
+
+  defp send_result(conn) do
+    conn.body_params
+    |> Map.get("q")
+    |> Graphqexl.Query.parse
+    |> Graphqexl.Query.execute(conn.assigns[:schema])
+    |> serialize
+    |> respond(conn, 200)
+  end
+
+  defp respond(response, conn, status) do
+    conn |> send_resp(status, response)
+  end
+
+  defp serialize(result) do
+    result
+    |> (&(Jason.encode(%{data: &1.data, errors: &1.errors}))).()
+    |> elem(1)
   end
 end
